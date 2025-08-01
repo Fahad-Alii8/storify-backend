31dc4914ab8042e967cfadcc3123b1a1c69e85e8
Fahad Ali <mfahadali.sparkingasia@gmail.com>
2025-08-02 00:44:31 +0500

    chore: db setup

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
